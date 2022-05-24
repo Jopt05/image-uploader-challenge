8ed757baf324cdbd0a466b4be10987628649db5d
Jesus Puentes <jopt98@gmail.com>
2022-05-24 16:43:08 -0500

    Modified middlewares and removed documents

--- middlewares/validar-archivo.js
+++ middlewares/validar-archivo.js
@@ -2,9 +2,19 @@ const { response } = require("express");
 
 const validarArchivoSubir = (req, res = response, next) => {
 
+    const archivosValidos = ['png', 'jpeg', 'jpg']
+
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
         return res.status(400).json({
-            msg: 'No hay archivos que subir'
+            msg: 'There is no files'
+        })
+    }
+
+    const splitName = req.files.archivo.name.split('.');
+
+    if( !archivosValidos.includes( splitName[ splitName.length - 1 ].toLowerCase() ) ) {
+        return res.status(400).json({
+            msg: 'Not a valid extension!'
         })
     }
 
